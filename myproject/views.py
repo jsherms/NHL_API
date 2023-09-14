02f7b8eed25545cbc52f0821fec049df55171c77
Joshua Sherman <73862925+jsherms@users.noreply.github.com>
2023-09-14 14:47:25 -0400

    Switching to Flask

--- myproject/views.py
+++ myproject/views.py
@@ -6,13 +6,27 @@ import pandas
 import matplotlib.pyplot as plt
 import numpy as np
 
-def generate_graph(request):
+
+def click_submit(request):
     if request.method == 'POST':
            selected_team = request.POST.get('teams')
            selected_type = request.POST.get('type')
            selected_filter = request.POST.get('filter')
+    import sqlite3
 
-    teams = ['Detroit Red Wings', 'New York Rangers', 'New York Islanders', 'Chicago Blackhawks']
+    # Connect to the hockey_data database
+    conn = sqlite3.connect('myproject/hockey_data.sqlite3')
+    cursor = conn.cursor()
+
+    # Retrieve all team names from the nhl_team table
+    cursor.execute("SELECT name FROM nhl_team")
+    teams = [row[0] for row in cursor.fetchall()]
+    teams.sort()
+
+    # Close the database connection
+    cursor.close()
+    conn.close()
+    
     types = ['schedule', 'data']
     filters = ['home only', 'away only', 'all']
     # Rest of your code here...
