55f402ceff7b0e80ea0b9767c1aefa1e3b73f96c
Joshua Sherman <73862925+jsherms@users.noreply.github.com>
2023-09-18 16:55:35 -0400

    Begun Working on Data

--- myproject/app.py
+++ myproject/app.py
@@ -10,7 +10,7 @@ cursor.execute("SELECT name FROM nhl_team")
 teams = [row[0] for row in cursor.fetchall()]
 teams.sort()
 
-    # Close the database connection
+# Close the database connection
 cursor.close()
 conn.close()
 
@@ -45,52 +45,51 @@ def submit():
 
         conn = sqlite3.connect('hockey_data.sqlite3')
         cursor = conn.cursor()
-        print(selected_team)
-        print(selected_type)
+        
         cursor.execute("SELECT id FROM nhl_team WHERE name = ?", (selected_team,))
         team_id = cursor.fetchone()
-        print(team_id[0])
     # Connect to the hockey_data database
         if selected_type == 'Schedule':
-            print("test2")
             api_endpoint = 'https://statsapi.web.nhl.com/api/v1/schedule?teamId=' + str(team_id[0]) + '&startDate=2023-10-10&endDate=2024-04-18'
             response = requests.get(api_endpoint)
             schedule = []
             if response.status_code == 200:
                 data = response.json()
-                for game_number in range(82):
-                    matchup = data['dates'][game_number]['games'][0]['teams']
-                    if matchup['away']['team']['name'] != selected_team:
-                        schedule.append(('Game ' + str(game_number+1) + ' ' + data['dates'][game_number]['date'] + ' vs. ' + matchup['away']['team']['name']))
-                    else:
-                        schedule.append(('Game ' + str(game_number+1) + ' ' + data['dates'][game_number]['date'] + ' @ ' + matchup['home']['team']['name']))
-                return render_template('result.html',  schedule=schedule)
+                for game_number in range(82):                
+                    game_info = {
+                        'Game Number': game_number + 1,
+                        'Game Date': data['dates'][game_number]['date'],
+                        'Home Team': data['dates'][game_number]['games'][0]['teams']['home']['team']['name'],
+                        'Away Team': data['dates'][game_number]['games'][0]['teams']['away']['team']['name']
+                    }
+                    if selected_filter == 'Home Only':
+                        if game_info['Home Team'] != selected_team:
+                            continue
+                    elif selected_filter == 'Away Only':
+                        if game_info['Away Team'] != selected_team:
+                            continue
+                    schedule.append(game_info)
+                return render_template('result.html',  schedule=schedule, teams=teams, types=types, filters=filters, default_team=selected_team, default_type=selected_type, default_filter=selected_filter)
             else:
                 print("Request failed with status code:", response.status_code)
-
+        else:
+            cursor.execute('SELECT * FROM player WHERE team_id = ?', (team_id[0],))
+            data = cursor.fetchall()
+            roster = []
+            for player in data:
+                player = {
+                    'Name': player[1],
+                    'Position': player[2],
+                    'Jersey Number': player[3]
+                }
+                roster.append(player)
+                roster = sorted(roster, key = lambda x: x['Jersey Number'])
+            return render_template('roster.html', roster=roster, teams=teams, types=types, filters=filters, default_team=selected_team, default_type=selected_type, default_filter=selected_filter )
     return redirect('/')
 
-@app.route('/schedule', methods=['GET'])
-def schedule():
-    if request.form.get('type') == 'Schedule':
-        print("test2")
-        api_endpoint = 'https://statsapi.web.nhl.com/api/v1/schedule?teamId=' + str(team_id) + '&startDate=2023-10-10&endDate=2024-04-18'
-        response = requests.get(api_endpoint)
-        schedule = []
-        if response.status_code == 200:
-            data = response.json()
-            for game_number in range(82):
-                matchup = data['dates'][game_number]['games'][0]['teams']
-                if matchup['away']['team']['name'] != selected_team:
-                    schedule.append(('Game ' + str(game_number+1), data['dates'][game_number]['date'] + 'vs. ' + matchup['away']['team']['name']))
-                else:
-                    schedule.append(('Game ' + str(game_number+1) + data['dates'][game_number]['date'] + '@ ' + matchup['home']['team']['name']))
-            print(schedule)
-            return render_template('result.html',  schedule=schedule)
-        else:
-            print("Request failed with status code:", response.status_code)
-
-    return render_template('result.html', team=teams, types=types, filters=filters)
+@app.route('/<path:path>')
+def catch_all(path='index.html'):
+    return render_template(path)
 
 if __name__ == '__main__':
     app.run()
